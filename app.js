5918ce8641da1bf36891b107425a984a3746bf86
Aun-Ali-12 <aunali29331@gmail.com>
2025-06-04 02:18:55 +0500

    designing remaining

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+function rollDice(){
 let dice = Math.floor(Math.random()*6+1)
-console.log(dice);
-
+// console.log(dice);
+let diceDiv = document.getElementById("dice")
+diceDiv.innerHTML = ""
+let p = document.createElement("p")
+p.innerText = dice
+diceDiv.append(p)
+}
